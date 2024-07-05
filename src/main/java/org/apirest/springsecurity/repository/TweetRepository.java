da27e489985a9f5fa3f30f3dadda518c78587a6a
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-07-05 17:57:08 -0300

    created repositories for entities

---
+++ src/main/java/org/apirest/springsecurity/repository/TweetRepository.java
@@ -0,0 +1,9 @@
+package org.apirest.springsecurity.repository;
+
+import org.apirest.springsecurity.entities.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+}
---
+++ src/main/java/org/apirest/springsecurity/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.apirest.springsecurity.repository;
+
+import org.apirest.springsecurity.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

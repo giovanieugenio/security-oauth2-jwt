6b22121d3accf3c83045e833997f5a72dbd8023e
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-07-05 17:33:23 -0300

    added enum Role for users type

--- src/main/java/org/apirest/springsecurity/entities/Role.java
+++ src/main/java/org/apirest/springsecurity/entities/Role.java
@@ -8,7 +8,40 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Long roleId;
 
     private String name;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Values {
+
+        ADMIN(1l),
+        BASIC(2l);
+
+        long roleId;
+
+        Values(long roleId){
+            this.roleId = roleId;
+        }
+
+        public long getRoleId() {
+            return roleId;
+        }
+    }
 }

54b43eceef210bf49779105e7a6ad0e4857ec5cc
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-07-05 18:11:09 -0300

    added docker-compose

--- src/main/java/org/apirest/springsecurity/entities/Tweet.java
+++ src/main/java/org/apirest/springsecurity/entities/Tweet.java
@@ -1,12 +1,16 @@
 package org.apirest.springsecurity.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
 
-@Table(name = "tb_tweeters")
+@Table(name = "tb_tweets")
 public class Tweet {
 
     @Id
--- src/main/java/org/apirest/springsecurity/entities/User.java
+++ src/main/java/org/apirest/springsecurity/entities/User.java
@@ -1,6 +1,15 @@
 package org.apirest.springsecurity.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 import java.util.Set;
 import java.util.UUID;

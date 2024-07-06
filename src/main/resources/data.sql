a26ac1aa6dd57951fc6693cf6e838bf26edc3f9c
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-07-05 21:44:53 -0300

    configured spring security for jwt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_roles(role_id, name) VALUES (1, "ADMIN");
-INSERT INTO tb_roles(role_id, name) VALUES (2, "BASIC");
+INSERT IGNORE INTO tb_roles(role_id, name) VALUES (1, "ADMIN");
+INSERT IGNORE INTO tb_roles(role_id, name) VALUES (2, "BASIC");

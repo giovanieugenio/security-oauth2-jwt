342decfda8b1c3befe76f8a2d7775dbb3401be29
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-07-08 14:53:24 -0300

    implementation of the create new user and listing methods

---
+++ src/main/java/org/apirest/springsecurity/controller/UserController.java
@@ -0,0 +1,62 @@
+package org.apirest.springsecurity.controller;
+
+import org.apirest.springsecurity.controller.dto.CreateUserDto;
+import org.apirest.springsecurity.entities.Role;
+import org.apirest.springsecurity.entities.User;
+import org.apirest.springsecurity.repository.RoleRepository;
+import org.apirest.springsecurity.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository,
+                          RoleRepository roleRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Transactional
+    @PostMapping("/users")
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto dto){
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+        var userFromDb = userRepository.findByUsername(dto.username());
+        if (userFromDb.isPresent()){
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        var user = new User();
+        user.setUsername(dto.username());
+        user.setPassword(bCryptPasswordEncoder.encode(dto.password()));
+        user.setRoles(Set.of(basicRole));
+        userRepository.save(user);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public ResponseEntity<List<User>> listUsers(){
+        var users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+}
